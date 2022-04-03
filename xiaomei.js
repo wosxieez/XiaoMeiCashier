ada01a698a31617a42aa8029fd05e1093c6dfeae
xez <xez@ipms.cn>
2022-04-03 15:01:30 +0800

    chore: update program mapping

---
+++ xiaomei.js
@@ -0,0 +1,11 @@
+export default {
+  xiaomeicashier: 'https://github.com/wosxieez/XiaoMeiCashier', // public
+  xiaomei: 'https://github.com/wosxieez/xiaomei', // 小美收银前端 private
+  xiaomeiapi: 'https://github.com/wosxieez/xiaomei-api', // 小美收银服务端 private
+  xiaomeidesktop: 'https://github.com/wosxieez/xiaomei-desktop', // 小美收银桌面客户端 private
+  xiaomeiwx: 'https://github.com/wosxieez/xiaomei-wx', // 小美收银微信公众号h5页面 private
+  xiaomeiwxapi: 'https://github.com/wosxieez/xiaomei-wx-api', // 小美收银微信公众号服务 private
+  xiaomeiallpayapi: 'https://github.com/wosxieez/xiaomei-all-pay-api', // 小美收银支付服务 private
+  xiaomeiforwardapi: 'https://github.com/wosxieez/xiaomei-forward-api', // 小美收银转发服务 (多服务器用户转发) private
+  xiaomeiagentapi: 'https://github.com/wosxieez/xiaomei-agent-api' // 小美收银代理服务 (代理商关系维护) private
+}
